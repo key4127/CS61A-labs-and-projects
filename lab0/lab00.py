8d1374ba3924f466c3ea666244d6a456edc43ff0
key4127 <149650562+key4127@users.noreply.github.com>
2024-03-28 21:02:04 +0800

    Add files via upload

---
+++ lab0/lab00.py
@@ -0,0 +1,22 @@
+def twenty_twenty():    #function definition
+    """Come up with the most creative expression that evaluates to 2020,    # single-line comment
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    
+    return 2020
+
+# single-line comment
+
+"""
+multi-line comment
+"""
+
+"""
+/ division (result is a floating point number)
+// integer division
+% modulus (remainder)
+** exponentiation
+"""
